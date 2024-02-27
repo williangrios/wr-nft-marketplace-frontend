c0937b5596517a36abf5161ca8f16062f60f5044
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2024-02-27 09:57:04 -0300

    feat: add logo image

--- src/img/index.ts
+++ src/img/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
+import logo from "../../public/img/logo.png"
 import nft_image_1 from "../../public/img/nft_image_1.png"
 
 export default {
-    nft_image_1
+    nft_image_1,
+    logo
 }
