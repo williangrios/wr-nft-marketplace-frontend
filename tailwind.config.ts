fe4a35f0b2df3b8cb0981d49ac30d5aae4d57a00
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2024-02-28 12:06:06 -0300

    feat: add personalized shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        "right-bottom": "10px 10px 10px rgba(0, 0, 0, 0.1)",
+      },
     },
   },
   plugins: [],
