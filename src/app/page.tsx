5b8115fcfa7b635781f66e58c6f3b8a356eb7e46
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2024-03-02 10:13:59 -0300

    feat: add new components to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import BigNFTSlider from "@/components/BigNFTSlider/BigNFTSlider";
+import Category from "@/components/Category/Category";
+import Filter from "@/components/Filter/Filter";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import Service from "@/components/Service/Service";
+import Subscribe from "@/components/Subscribe/Subscribe";
+import Title from "@/components/Title/Title";
 
 export default function Home () {
   return (
     <main>
       <HeroSection />
       <Service />
+      <BigNFTSlider />
+      <Title title="Featured NFTs" subtitle="Discover the most outstanding NFTs in all topics of life." />
+      <Filter />
+      <Title title="Browse by category" subtitle="Exlpore the NFTs in the most featured categories." />
+      <Category />
+      <Subscribe />
     </main>
   );
 }
