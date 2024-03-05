db86f0f36d27d66c49dbaa230df1ebadb6013f1b
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2024-03-05 08:42:02 -0300

    feat: add NFTCard Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import BigNFTSlider from "@/components/BigNFTSlider/BigNFTSlider";
 import Category from "@/components/Category/Category";
 import Filter from "@/components/Filter/Filter";
 import HeroSection from "@/components/HeroSection/HeroSection";
+import NFTCard from "@/components/NFTCard/NFTCard";
 import Service from "@/components/Service/Service";
 import Subscribe from "@/components/Subscribe/Subscribe";
 import Title from "@/components/Title/Title";
@@ -14,6 +15,7 @@ export default function Home () {
       <BigNFTSlider />
       <Title title="Featured NFTs" subtitle="Discover the most outstanding NFTs in all topics of life." />
       <Filter />
+      <NFTCard />
       <Title title="Browse by category" subtitle="Exlpore the NFTs in the most featured categories." />
       <Category />
       <Subscribe />
