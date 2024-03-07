3decd2c1a549f5330b0f711cf662317c8ed8468c
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2024-03-07 09:34:35 -0300

    feat: add components to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import AudioLive from "@/components/AudioLive/AudioLive";
 import BigNFTSlider from "@/components/BigNFTSlider/BigNFTSlider";
 import Category from "@/components/Category/Category";
+import Collection from "@/components/Collection/Collection";
 import Filter from "@/components/Filter/Filter";
+import FollowerTab from "@/components/FollowerTab/FollowerTab";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import NFTCard from "@/components/NFTCard/NFTCard";
 import Service from "@/components/Service/Service";
@@ -13,6 +16,11 @@ export default function Home () {
       <HeroSection />
       <Service />
       <BigNFTSlider />
+      <Title title="Latest Audio Collection" subtitle="Discover amazing audios." />
+      <AudioLive />
+      <Title title="New Collection" subtitle="Discover amazing things by collection." />
+      <FollowerTab />
+      <Collection />
       <Title title="Featured NFTs" subtitle="Discover the most outstanding NFTs in all topics of life." />
       <Filter />
       <NFTCard />
