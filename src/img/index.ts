6192cdb8c461123bf4451d7a6a42075a2277e5b2
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2024-03-10 19:36:56 -0300

    feat: add new images

--- src/img/index.ts
+++ src/img/index.ts
@@ -1,8 +1,22 @@
 /* eslint-disable import/no-anonymous-default-export */
 import logo from "../../public/img/logo.png"
+import hero_image from "../../public/img/hero_image.jpg"
+import nft_image from "../../public/img/nft_image.jpg"
 import nft_image_1 from "../../public/img/nft_image_1.png"
+import videos from "../../public/img/videos.png"
+import nft_1 from "../../public/img/nft_1.png"
+import update from "../../public/img/update.jpg"
+import earn from "../../public/img/earn.jpg"
+import categoryBackground1 from "../../public/img/categoryBackground1.jpg"
 
 export default {
+    nft_image,
     nft_image_1,
-    logo
+    nft_1,
+    earn,
+    videos,
+    update,
+    logo,
+    hero_image,
+    categoryBackground1
 }
