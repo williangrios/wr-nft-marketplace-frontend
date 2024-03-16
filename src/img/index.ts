d26b7c064ee928cdbd062646edc060551c522fba
Willian Rios <williangrios@yahoo.com.br>
2024-03-16 09:43:16 -0300

    feat: add new images

--- src/img/index.ts
+++ src/img/index.ts
@@ -5,9 +5,12 @@ import nft_image from "../../public/img/nft_image.jpg"
 import nft_image_1 from "../../public/img/nft_image_1.png"
 import videos from "../../public/img/videos.png"
 import nft_1 from "../../public/img/nft_1.png"
+import upload from "../../public/img/upload.png"
 import update from "../../public/img/update.jpg"
 import earn from "../../public/img/earn.jpg"
 import categoryBackground1 from "../../public/img/categoryBackground1.jpg"
+import provider1 from "../../public/img/provider1.png"
+import provider2 from "../../public/img/provider2.png"
 
 export default {
     nft_image,
@@ -15,8 +18,11 @@ export default {
     nft_1,
     earn,
     videos,
+    upload,
     update,
     logo,
     hero_image,
-    categoryBackground1
+    categoryBackground1,
+    provider1,
+    provider2
 }
