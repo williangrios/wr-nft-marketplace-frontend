0b120a051e63911a7b30b6510e9f4bf938fdaf83
Willian Rios <williangrios@yahoo.com.br>
2024-04-08 22:59:23 -0300

    fix: fix bugs

--- src/app/(pages)/NftDetails/page.tsx
+++ src/app/(pages)/NftDetails/page.tsx
@@ -1,57 +1,3 @@
-// "use client";
-// import React, { useEffect, useState, useContext } from "react";
-// import { useSearchParams } from "next/navigation";
-// import Category from "@/components/Category/Category";
-// import Brand from "@/components/Brand/Brand";
-// import NFTDetailsPage from "./components/NFTDetailsPage";
-// import { WRNFTMarketplaceContext } from "@/context/WRNFTMarketplaceContext";
-
-// function NFTDetails() {
-//   const { currentAccount } = useContext(WRNFTMarketplaceContext);
-//   const [nft, setNft] = useState<any>({
-//     image: "",
-//     tokenId: "",
-//     name: "",
-//     description: "",
-//     owner: "",
-//     price: "",
-//     seller: "",
-//   });
-
-//   const params = useSearchParams();
-
-//   useEffect(() => {
-//     if (!params) return;
-//     const image = params.get("image");
-//     const tokenId = params.get("tokenId");
-//     const name = params.get("name");
-//     const owner = params.get("owner");
-//     const description = params.get("description");
-//     const price = params.get("price");
-//     const seller = params.get("seller");
-//     const nftData = {
-//       image,
-//       tokenId,
-//       name,
-//       owner,
-//       description,
-//       price,
-//       seller,
-//     };
-//     setNft(nftData);
-//   }, [params]);
-
-//   return (
-//     <div>
-//       <NFTDetailsPage nft={nft} />
-//       <Category />
-//       <Brand />
-//     </div>
-//   );
-// }
-
-// export default NFTDetails;
-
 "use client";
 import React, { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
@@ -60,7 +6,6 @@ import Brand from "@/components/Brand/Brand";
 import NFTDetailsPage from "./components/NFTDetailsPage";
 
 function NFTDetails() {
-  // Suspense para envolver a chamada useSearchParams
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <NFTDetailsContent />
